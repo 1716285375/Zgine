8155b6b3eb1c2b0c8d9de56b110f53aaea78836a
1716285375 <1716285375@qq.com>
2025-08-12 11:50:34 +0800

    update build script

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -1,8 +1,11 @@
 @echo off
-echo Performing cleanup...
-call CleanProject.bat  :: First call Clean.bat to perform cleanup
+REM clean project script
+::echo Performing cleanup...
+::call CleanProject.bat  :: First call Clean.bat to perform cleanup
 
-echo Cleanup completed, starting project generation...
+::echo Cleanup completed, starting project generation...
+
+REM generate project script
 call vendor\bin\premake\premake5.exe vs2022  :: Then call Premake to generate solution
 
 echo ========== Generation Done ============
