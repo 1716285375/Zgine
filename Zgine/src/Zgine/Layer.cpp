71260e59b33d25bed8dcdbe2b4ec62d2a5e0f3db
1716285375 <1716285375@qq.com>
2025-08-18 21:01:34 +0800

    add layer module to engine

---
+++ Zgine/src/Zgine/Layer.cpp
@@ -0,0 +1,13 @@
+#include "zgpch.h"
+#include "Layer.h"
+
+
+namespace Zgine {
+
+
+	Layer::Layer(const std::string& name /*= "Layer"*/)
+	{
+
+	}
+
+}
---
+++ Zgine/src/Zgine/Layer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Zgine/Core.h>
+#include <Zgine/Events/Event.h>
+
+
+namespace Zgine {
+	class ZG_API Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer() {};
+
+		virtual void OnAttach() {};
+		virtual void OnDetach() {};
+		virtual void OnUpdate() {};
+		virtual void OnEvent(Event& event) {};
+
+		inline const std::string& GetName() const { return m_DebugName; };
+
+	private:
+		std::string m_DebugName;
+	};
+}
+
